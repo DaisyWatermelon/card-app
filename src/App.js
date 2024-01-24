efb86c699d1d9aa1c83591b04db2815fe9696ad2
n10362428 <n10362428@QUT-PA00152680>
2024-01-24 12:02:42 +1000

    unfinished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import logo from './logo.svg';
+import {create} from 'zustand'
 import './App.css';
 import Card1 from './Card1'
 import Card2 from './Card2'
+import myToken from './myToken';
+import { createStore } from 'zustand';
 
+const useStore = create<createStore>((set) =>({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 }))
+}))
+
+function CardCounter(){
+  const {count, increment} = useStore();
+  return(
+    <div>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
+
+function Controls(){
+  const addCard = useStore((state) => state.addCard);
+
+  const handleAddCard = () => {
+    const cardToAdd = <myToken />
+
+    addCard={cardToAdd};
+  };
+  return <button onClick={handleAddCard}>Add Card1</button>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card1 />
+        <CardCounter />
+        <Controls />
+        
+        <button onClick={increment}><Card1 /></button>
         <Card2 />
+        {useStore((state) => state.cards)}
+        <myToken />
       </header>
     </div>
   );
--- src/myToken.js
+++ src/myToken.js
@@ -1,24 +1,13 @@
 import React from 'react'
-import {create} from 'zustand'
 
-const useStore = create((set) =>({
-    cards: 2,
-    increasePopulation: () => set((state) => ({ cards: state.cards + 1 }))
-}))
 
-function CardCounter(){
-    const cards = useStore((state) =>state.cards)
-    return <h1>{cards} around here...</h1>
-}
 
-function Controls(){
-    const increasePopulation = useStore((state) => state.increasePopulation)
-    return <button onClick={increasePopulation}>Add card</button>
-}
 
 export default function myToken() {
     
   return (
-    <div>myToken</div>
+    <div>
+        <h1>My favourite token list</h1>
+    </div>
   )
 }
