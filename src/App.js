f740c6812b8720a932d3b8c5dd3698c7f6cf4a8d
n10362428 <n10362428@QUT-PA00152680>
2024-01-26 10:55:59 +1000

    adding card to my token

--- src/App.js
+++ src/App.js
@@ -3,56 +3,81 @@ import {create} from 'zustand'
 import './App.css';
 import Card1 from './Card1'
 import Card2 from './Card2'
-import myToken from './myToken';
+//import myToken from './myToken';
 import { createStore } from 'zustand';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-const useStore = create((set) => ({
+const useStore = create(set => ({
   count: 0,
   cards: [], // To store favorited cards
-  addCard: (card) => set((state) => ({ cards: [...state.cards, card] })),
-  increment: () => set((state) => ({ count: state.count + 1 })),
+  addCard: () => set(state => ({ count: state.count + 1 })),
+  //removeCard: () => set(state => ({ count: state.count - 1 })),
 }));
 
-function CardCounter(){
-  const {count, increment} = useStore();
-  return(
-    <div>
-      <button onClick={increment}>Increment</button>
-    </div>
-  )
-}
+// function CardCounter(){
+//   const {count, increment} = useStore();
+//   return(
+//     <div>
+//       <button onClick={increment}>Increment</button>
+//     </div>
+//   )
+// }
 
-function Controls(){
-  const addCard = useStore((state) => state.addCard);
+// function ControlsCard1(){
+//   const addCard = useStore((state) => state.addCard);
 
-  const handleAddCard = () => {
-    const cardToAdd = <myToken />
+//   const handleAddCard = () => {
+//     const cardToAdd = <myToken />
 
-    addCard={cardToAdd};
-  };
-  return <button onClick={handleAddCard}>Add Card1</button>;
-}
+//     addCard={cardToAdd};
+//   };
+//   return <button onClick={handleAddCard}><Card1 /></button>;
+// }
+
+// function ControlsCard2(){
+//   const addCard = useStore((state) => state.addCard);
+
+//   const handleAddCard = () => {
+//     const cardToAdd = <myToken />
+
+//     addCard={cardToAdd};
+//   };
+//   return <button onClick={handleAddCard}><Card2 /></button>;
+// }
+
+// function myToken(){
+//   const getCards = useStore(state => state.card);
+//   return (
+//     <div>
+//         <h1>My favourite token list</h1>
+//         <h2>
+//           {getCards}
+//         </h2>
+//     </div>
+//   )
+//}
 
 function App() {
-  const {count, cards, addCard, increment} = useStore();
+  const addCard = useStore(state => state.addCard);
+  const getCards = useStore(state => state.count);
   return (
-    <Router>
+    // <Router>
       <div className="App">
         <header className="App-header">
-          <CardCounter />
-          <Controls />
+          {/* <CardCounter /> */}
+          {/* <ControlsCard1 />
+          <ControlsCard2 /> */}
+          {/* <Link to="/"><Card1 /></Link>
+          <Link to="/"><Card2 /></Link>
+          <Link to="/myToken"><myToken /></Link> */}
+          <h1>{getCards} my favourite card</h1>
+          <button onClick={addCard}><Card1 /></button>
+          <button onClick={addCard}><Card2 /></button>
 
-          <Link to="/"><Card1 /></Link>
-          <Card2 />
-          {useStore((state) => state.cards)}
-          <myToken />
         </header>
       </div>
-      <p>Count: {count}</p>
-      <button onClick={addCard}><Card1 /></button>
-      <button onClick={addCard}><Card2 /></button>
-    </Router>
+      
+    // </Router>
     
   );
 }
--- src/myToken.js
+++ src/myToken.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import create from 'zustand';
+
 
 export default function myToken() {
     
   return (
     <div>
         <h1>My favourite token list</h1>
+        <h2></h2>
     </div>
   )
 }
