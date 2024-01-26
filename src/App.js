b81e002ec12ebb3129c6a6ded1927055d92c236b
n10362428 <n10362428@QUT-PA00152680>
2024-01-26 13:25:11 +1000

    add and remove cards button to add and remove specific card

--- src/App.js
+++ src/App.js
@@ -1,81 +1,62 @@
 import logo from './logo.svg';
+import React, { useRef } from 'react';
 import {create} from 'zustand'
+import {persist} from "zustand/middleware"
 import './App.css';
 import Card1 from './Card1'
 import Card2 from './Card2'
 //import myToken from './myToken';
 import { createStore } from 'zustand';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Card from './Card1';
 
 const useStore = create(set => ({
   count: 0,
   cards: [], // To store favorited cards
-  addCard: () => set(state => ({ count: state.count + 1 })),
+  addCard: (card) => set(state => ({cards: [...state.cards, card] })),
   removeCard: () => set(state => ({ count: state.count - 1 })),
 }));
 
-// function CardCounter(){
-//   const {count, increment} = useStore();
-//   return(
-//     <div>
-//       <button onClick={increment}>Increment</button>
-//     </div>
-//   )
-// }
-
-// function ControlsCard1(){
-//   const addCard = useStore((state) => state.addCard);
-
-//   const handleAddCard = () => {
-//     const cardToAdd = <myToken />
-
-//     addCard={cardToAdd};
-//   };
-//   return <button onClick={handleAddCard}><Card1 /></button>;
-// }
-
-// function ControlsCard2(){
-//   const addCard = useStore((state) => state.addCard);
-
-//   const handleAddCard = () => {
-//     const cardToAdd = <myToken />
-
-//     addCard={cardToAdd};
-//   };
-//   return <button onClick={handleAddCard}><Card2 /></button>;
-// }
-
-// function myToken(){
-//   const getCards = useStore(state => state.card);
-//   return (
-//     <div>
-//         <h1>My favourite token list</h1>
-//         <h2>
-//           {getCards}
-//         </h2>
-//     </div>
-//   )
-//}
-
 function App() {
-  const addCard = useStore(state => state.addCard);
+  const cards = useStore((state) => state.cards);
+  const addCards = useStore(state => state.addCard);
+  const inputRef = useRef();
+  const addCard = () =>{
+    addCards(inputRef.current.value);
+    inputRef.current.value = "";
+  };
+  // const addCard = () =>{
+  //   addCards(<Card1/>);
+  // };
   const removeCard = useStore(state => state.removeCard);
-  const getCards = useStore(state => state.count);
+  // const getCards = useStore(state => state.count);
   return (
     // <Router>
       <div className="App">
         <header className="App-header">
-          {/* <CardCounter /> */}
-          {/* <ControlsCard1 />
-          <ControlsCard2 /> */}
-          {/* <Link to="/"><Card1 /></Link>
-          <Link to="/"><Card2 /></Link>
-          <Link to="/myToken"><myToken /></Link> */}
-          <h1>{getCards} my favourite card</h1>
-          <button onClick={addCard}><Card1 /></button>
-          <button onClick={addCard}><Card2 /></button>
+          <Card1 />
+          <Card2 />
+          <h1> my favourite card</h1>
+          <input ref={inputRef} />
+          <button onClick={addCards}>Add card 1</button>
+          {cards.map((card) =>(
+            <p key={card}>{card}</p>
+          ))}
+          <button onClick={addCards}>Add card 2</button>
+          {cards.map((card) =>(
+            <p key={card}>{card}</p>
+          ))}
+          <button onClick={removeCard}>Remove card 2</button>
+          {cards.map((card) =>(
+            <p key={card}>{card}</p>
+          ))}
+          <button onClick={removeCard}>Remove card 2</button>
+          {cards.map((card) =>(
+            <p key={card}>{card}</p>
+          ))}
+          {/* <button onClick={addCards}><Card2 /></button>
           <button onClick={removeCard}>Remove Card1</button>
-          <button onClick={removeCard}>Remove Card2</button>
+          <button onClick={removeCard}>Remove Card2</button> */}
 
         </header>
       </div>
